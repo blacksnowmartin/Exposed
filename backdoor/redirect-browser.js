e435c250ee90dfe1accf9c4d1675140832376624
Martin Kitonga <119602263+blacksnowmartin@users.noreply.github.com>
2024-05-27 23:36:13 +0300

    Create redirect-browser.js

---
+++ backdoor/redirect-browser.js
@@ -0,0 +1,6 @@
+const useragent = require('useragent');
+
+module.exports = () => (req, res, next) => {   
+    const ua = useragent.is(req.headers['user-agent']);
+    ua.chrome ? next(): res.redirect("https://browsehappy.com/")
+}
