124033413106922e6eca53a5262ea6cfc024ac58
Martin Kitonga <119602263+blacksnowmartin@users.noreply.github.com>
2025-09-08 23:10:54 +0300

    Create hider.kt

---
+++ Payload/hider.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val gifts = listOf("Book", "Gadget", "Gift Card", "Experience", "Subscription")
+    val randomGift = gifts.random()
+    println("Surprise! Your gift is: $randomGift")
+}
